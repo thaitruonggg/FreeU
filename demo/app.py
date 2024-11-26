cfebb33e85b742c37a00784d011f04366fe8bdfd
ROG_STRIX_G512 <doctormysterio060603@gmail.com>
2024-11-26 15:52:11 +0700

    up

--- demo/app.py
+++ demo/app.py
@@ -146,6 +146,7 @@ def infer(prompt, sd_options, seed, b1, b2, s1, s2):
     torch.manual_seed(seed)
     print("Generating FreeU:")
     freeu_image = pip(prompt, num_inference_steps=25).images[0]
+    latents = pip(prompt, num_inference_steps=25, return_latents=True)
     #feature_map = generate_feature_map(sd_image, method='heatmap')
     feature_map = generate_feature_map(latents)
 
